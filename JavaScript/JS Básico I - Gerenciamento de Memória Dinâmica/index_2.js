93d87a47fe121fae3886d35b4465e1c0bf0f9db4
ClecianoPedro <itzcleciano@gmail.com>
2024-12-28 17:47:44 -0300

    JS Básico I - Operadores matemáticos e Precedência dos operadores

---
+++ JavaScript/JS Básico I - Gerenciamento de Memória Dinâmica/index_2.js
@@ -0,0 +1,19 @@
+// soma = +
+// subtração = -
+// multiplicação (diferente da matemática) = *
+// divisão = /
+// resto = %
+// exponenciação = **
+
+// Precedência dos operadores
+// parenteses, exponenciação, multiplicação e divisão, soma e subtração, mesma regra da álgebra 
+// mediaDasNotas = (nota1 + nota2) / 2;
+
+// Praticando
+const nota1 = 10;
+const nota2 = 8;
+
+const somaDasNotas = nota1 + nota2;
+let mediaDasNotas = somaDasNotas / 2;
+console.log("Soma das notas =", somaDasNotas); // Imprimindo texto com variáveis, a ',' já adiciona um espaço após ela
+console.log("Média das notas =", mediaDasNotas);
