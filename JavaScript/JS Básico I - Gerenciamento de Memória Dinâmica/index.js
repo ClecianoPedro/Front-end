676d3271656c3a1d2baeae0265fe54c0852b2274
ClecianoPedro <itzcleciano@gmail.com>
2024-12-28 17:00:00 -0300

    JS Básico I - Tipagem, Declarando variáveis, Imprimindo informações, Tipos básicos de variáveis

--- JavaScript/JS Básico I - Gerenciamento de Memória Dinâmica/index.js
+++ JavaScript/JS Básico I - Gerenciamento de Memória Dinâmica/index.js
@@ -1,1 +1,23 @@
 console.log("Hello World!")
+
+// Padrão JS (Camel case)
+// Case sensitive
+// Tipagem dinâmica
+// Tipagem fraca
+// Não iniciar com números
+
+// Declarando variáveis
+var nomeDoAluno = "Cleciano Pedro"; // String
+var idade = 27; // Number
+var altura = 1.77; // Number
+var estudando = true; // Boolean, sempre minúsculo
+var semConteudo; // Undefined
+
+// Imprimindo variáveis
+console.log(nomeDoAluno); // Imprime a variável
+console.log(typeof nomeDoAluno); // Operador Unário, imprime o tipo da variável
+console.log(nomeDoAluno, typeof nomeDoAluno); // Imprimindo 2 ou mais informações com 1 linha
+console.log(idade, typeof idade);
+console.log(altura, typeof altura);
+console.log(estudando, typeof estudando);
+console.log(semConteudo, typeof semConteudo);
