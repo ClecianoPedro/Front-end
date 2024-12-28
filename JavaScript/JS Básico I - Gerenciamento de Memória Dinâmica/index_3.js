8c7e1327c5fe37c126116a78a57bce574cd9b3b2
ClecianoPedro <itzcleciano@gmail.com>
2024-12-28 19:09:23 -0300

    JS Básico I - Entrada de Dados (prompt do navegador e Node.JS), Introdução à Biblioteca readline-sync

---
+++ JavaScript/JS Básico I - Gerenciamento de Memória Dinâmica/index_3.js
@@ -0,0 +1,4 @@
+const prompt = require("readline-sync"); // Importando a biblioteca 'readline-sync' para utilizar prompts
+
+const idade = prompt.question("Qual é a sua idade?"); // Utilizando o método 'question' para utilizar o prompt
+console.log(idade); // Exibindo o que foi enviado no prompt
