ebdecf5ade1bf1a948f144821eb41df502227227
ClecianoPedro <itzcleciano@gmail.com>
2024-12-28 17:24:58 -0300

    JS Básico I - Formas de declarar variáveis após o ES6, adicionado arquivo .gitignore

--- JavaScript/JS Básico I - Gerenciamento de Memória Dinâmica/index.js
+++ JavaScript/JS Básico I - Gerenciamento de Memória Dinâmica/index.js
@@ -5,6 +5,8 @@ console.log("Hello World!")
 // Tipagem dinâmica
 // Tipagem fraca
 // Não iniciar com números
+// Não utilizar 'var' para declarar variáveis, pois se torna uma variável GLOBAL, já existe antes do fluxo da página mesmo sem ser declarada!
+// Não precisa colocar ';' no final de cada linha do código
 
 // Declarando variáveis
 var nomeDoAluno = "Cleciano Pedro"; // String
@@ -26,3 +28,11 @@ console.log(semConteudo, typeof semConteudo);
 var curso = "Front-end" , topico = "JavaScript Básico I";
 
 console.log(curso, topico);
+
+// Formas de declarar variáveis após o ES6
+let notaDoAluno = 10; // 'let' permite que a variável seja alterada depois
+const mediaDoAluno = 8; // 'const' não permite que a variável seja alterada depois
+notaDoAluno = 9; // Alterando a variável 'let'
+console.log(notaDoAluno);
+// mediaDoAluno = 5; Não é permitido 
+console.log(mediaDoAluno);
