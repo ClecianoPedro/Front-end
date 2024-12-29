f2e02d8c8cac34c359b00a8ee9c1998d4ec8c253
ClecianoPedro <itzcleciano@gmail.com>
2024-12-29 13:32:17 -0300

    Programação Assíncrona - ASYNC/AWAIT

--- JavaScript/Programação Assincrona/assincronismo.js
+++ JavaScript/Programação Assincrona/assincronismo.js
@@ -1,33 +1,34 @@
-const fs = require("fs");
+import fs from "fs";
 
 // Callbacks
 // Exemplo 1
 
 // Executa os 2 console.log() antes de ler o arquivo
-// console.log("ANTES da função de ler o arquivo");
-// // Função assíncrona (ler um arquivo leva um tempo)
-// fs.readFile("JavaScript/Programação Assincrona/arquivo.txt", (erro, conteudoDoArquivo) => {
-//     if (erro) {
-//         console.log("Ocorreu um erro ao tentar ler o arquivo:", erro);
-//     } else {
-//         console.log(String((conteudoDoArquivo)));
-//     }
-// });
+console.log("ANTES da função de ler o arquivo");
+// Função assíncrona (ler um arquivo leva um tempo)
+fs.readFile("JavaScript/Programação Assincrona/arquivo.txt", (erro, conteudoDoArquivo) => {
+    if (erro) {
+        console.log("Ocorreu um erro ao tentar ler o arquivo:", erro);
+    } else {
+        console.log(String((conteudoDoArquivo)));
+    }
+});
 
-// console.log("DEPOIS da função de ler o arquivo...");
+console.log("DEPOIS da função de ler o arquivo...");
 
 // Exemplo 2: setTimeout(função assíncrona que cria um timer, quando for finalizado o timer ele executa o que estiver dentro da função de callback)
 
-// console.log("ANTES do setTimeout...")
+console.log("ANTES do setTimeout...");
 
-// setTimeout(() => {
-//     console.log("Isso aqui vai ser executado após 2 segundos")
-// }, 2*1000) // 2ms*1000 = 2seg.
+setTimeout(() => {
+    console.log("Isso aqui vai ser executado após 2 segundos");
+}, 2*1000); // 2ms*1000 = 2seg.
 
-// console.log("DEPOIS do setTimeout...")
+console.log("DEPOIS do setTimeout...");
 
 
 // Promises
+// Exemplo de utilização: Pegar dados do back-end (API)
 
 // A promise é um objeto que guarda uma promessa de que algo será realizado, é útil para os casos em que estamos lidando com operações assíncronas.
 // A promise estará em algum dos estados abaixo:
@@ -35,9 +36,7 @@ const fs = require("fs");
 // 2. Fullfilled: A promise foi concretizada com sucesso.
 // 3. Rejected: A promise foi rejeitada, porque houve um erro.
 
-// Exemplo de utilização: Pegar dados do back-end (API)
-
-console.log("ANTES da criação da promise")
+// console.log("ANTES da criação da promise");
 // Criando função que retorna uma promise
 function lerAquivoPromise() {
     return new Promise((resolve, reject) => {
@@ -62,5 +61,22 @@ lerAquivoPromise()
     }).catch ((erro) => {
         console.log("Deu ruim:", erro);
     }).finally(() => {
-        console.log("Isso aqui vai ser executado independente do sucesso ou fracasso da promise, ao final dela")
-    })
+        console.log("Isso aqui vai ser executado independente do sucesso ou fracasso da promise, ao final dela");
+    });
+
+
+// async/await
+
+// async transforma a função em assíncrona
+// await só pode ser colocado dentro de uma função assíncrona
+
+async function leituraDeDados() {
+    console.log("Isso é executado antes da promise ser resolvida");
+
+    const retornoDaPromise = await lerAquivoPromise(); // Espera a promise ser resolvida para avançar no código
+
+    console.log(retornoDaPromise)
+    console.log("Isso é executado DEPOIS da promise ser resolvida");
+};
+
+leituraDeDados();
