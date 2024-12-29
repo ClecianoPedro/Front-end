bbf8255552d1adf513c3e498b48d1e179cf75fab
ClecianoPedro <itzcleciano@gmail.com>
2024-12-28 23:09:02 -0300

    JS Básico II - Operador Ternário (? e :)

---
+++ JavaScript/JS Básico II - Controle de Fluxo/condicionais_3.js
@@ -0,0 +1,11 @@
+// Operador Ternário
+
+const idade1 = 21;
+
+idade1 >= 18 ? console.log("Maior de idade") : console.log("Menor de idade")
+// Após o '?' o que acontece se for true e após o ':' o que acontece se for false
+
+const idade2 = 17;
+const mensagem = idade2 >= 18 ? "Maior de idade" : "Menor de idade";
+
+console.log(mensagem);
