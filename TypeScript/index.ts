64541f743323303c84644b1c1305a9656e70f22c
ClecianoPedro <itzcleciano@gmail.com>
2025-01-02 14:43:29 -0300

    TypeScript II - Tipando números, Strings e Booleanos

---
+++ TypeScript/index.ts
@@ -0,0 +1,7 @@
+let numero: number; // Tipagem do tipo
+let numero1 = 20; // Inferência de tipo
+const pi = 3.1415; // Constante tem o tipo igual ao valor dado à ela, pois o valor dela não pode ser alterado, um tipo 'number' por exemplo seria um número genérico
+
+let nome = "Pedro"; // Inferência de tipo
+
+let correta: boolean;
