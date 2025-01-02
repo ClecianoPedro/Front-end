a0659208c4df5603c3e9cfb5f088cb1c8cb12171
ClecianoPedro <itzcleciano@gmail.com>
2025-01-02 15:04:36 -0300

    TypeScript II - Tipagem de Arrays

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -4,4 +4,14 @@ const pi = 3.1415; // Constante tem o tipo igual ao valor dado à ela, pois o va
 
 let nome = "Pedro"; // Inferência de tipo
 
-let correta: boolean;
+let correta: boolean; // Inferência de tipo
+
+let resuldado = numero1 * pi; // Inferência de tipo
+
+const numeros: number[] = [1, 2, 3, 4, 5]; // Tipagem de array (forma mais comun)
+
+const numeros1: Array<number> = [1, 2, 3 ,4, 5]; // Tipagem de array (forma mais antiga)
+
+const misto: (number | string)[] = ["Pedro", 27, 1.83]; // Tipagem de array misto (não recomendado)
+
+const idades: number[] = []; // Iniciando array vazio
