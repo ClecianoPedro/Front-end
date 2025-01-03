7b92fffd269b2dd8b4b3f0c5f7cb3d9e67355251
ClecianoPedro <itzcleciano@gmail.com>
2025-01-03 17:03:25 -0300

    Browser API II - Manipulando Classes com JS

--- js/index_2.js
+++ js/index_2.js
@@ -10,17 +10,23 @@ increaseButton.addEventListener("click", (event) => {
     const valorAtual = Number(counterElement.textContent);
 
     counterElement.textContent = valorAtual + 1;
+
+    increaseButton.classList.add("btn");
+    decreaseButton.classList.remove("btn");
 }); // 1° evento que estará sendo escutado, 2° função que será executada quando ocorrer o evento
 
 decreaseButton.addEventListener("click", (event) => {
     const currentValue = Number(counterElement.textContent);
 
     counterElement.textContent = currentValue - 1;
-})
+
+    decreaseButton.classList.add("btn");
+    increaseButton.classList.remove("btn");
+});
 
 input.addEventListener("input", () => {
     console.log(input.value);
-})
+});
 
 // Adicionando estilos inline ao elemento "contador"
 
@@ -30,3 +36,21 @@ counterElement.style.backgroundColor = "#AAA"; // Utilizar CamelCase quando o no
 counterElement.style.border = "1px solid black"
 counterElement.style.width = "150px"
 
+// Manipulando classes
+
+const themeButton = document.querySelector("#theme");
+
+let darkTheme = false;
+
+themeButton.addEventListener("click", () => {
+    darkTheme = !darkTheme; // darkTheme recebe o contrário dela
+
+    const body = document.querySelector("body");
+    if (darkTheme) {
+    body.style.backgroundColor = "black";
+    body.style.color = "white";
+    } else {
+        body.style.backgroundColor = "white";
+        body.style.color = "black";
+    }
+});
