32441908f331724ee263912b5185e6352f4b7ba4
ClecianoPedro <itzcleciano@gmail.com>
2025-01-05 02:31:37 -0300

    React Legado III - Segundo Projeto React - Ciclo de Vida - Montagem - (constructor, componentWillMount, componentDidMount, render)

--- React/ciclo-de-vida/src/components/Counter/Counter.jsx
+++ React/ciclo-de-vida/src/components/Counter/Counter.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 export class Counter extends React.Component {
+    // Primeria etapa do ciclo de montagem
+    constructor() {
+        super()
+        console.log("Construindo a classe Counter...")
+    }
+    // Não utilizar componentWillMount()
+    UNSAFE_componentWillMount() {
+        console.log("O componente -contador- será montado!")
+    }
+
+    // Última etapa do ciclo de montagem
+    componentDidMount() {
+        console.log("O componente foi montado! ✅")
+    }
+    // Terceira etapa do ciclo de montagem
     render() {
+        console.log("Renderizando o componente Counter...")
         return (
             <div>
                 <h1>0</h1>
