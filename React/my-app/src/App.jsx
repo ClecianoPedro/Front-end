afa5cf207c1f56acb82589f3c507fee386525f5c
ClecianoPedro <itzcleciano@gmail.com>
2025-01-04 22:29:02 -0300

    React Legado I - Componentização

---
+++ React/my-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+// Componente em classe é uma classe que herda a classe Component do React, e retorna HTML dentro do método render().
+class App extends React.Component {
+  // Método responsável por renderizar o conteúdo HTML do nosso componente
+  render() {
+    return (
+      <header></header>
+      )
+  }
+}
+
+export default App;
