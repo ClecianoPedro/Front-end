1266026a5cc9f6235f3202124b22500d11106322
ClecianoPedro <itzcleciano@gmail.com>
2025-01-04 23:58:46 -0300

    React Legado I - Componentização - Criando Componente Navbar

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,27 +1,11 @@
 import React from 'react'
-import logoImg from './assets/images/logo.png'
+import Navbar from './components/Navbar/Navbar.jsx';
 
 // Componente em classe é uma classe que herda a classe Component do React, e retorna HTML dentro do método render().
 class App extends React.Component {
   // Método responsável por renderizar o conteúdo HTML do nosso componente
   render() {
-    return (
-      <header>
-        <nav>
-          <div className="nav-brand">
-            <img src={logoImg} alt="" />
-            <h1>Space Flight News</h1>
-          </div>
-
-          <ul className='nav-list'>
-            <li><a href="/">Home</a></li>
-            <li><a href="/">Trending</a></li>
-            <li><a href="/">Categories</a></li>
-            <li><a href="/">About us</a></li>
-          </ul>
-        </nav>
-      </header>
-      )
+    return <Navbar />
   }
 }
 
