6c8dec9730dc0b6f392c072bd5c0fb0fb536d4c7
ClecianoPedro <itzcleciano@gmail.com>
2025-01-04 22:38:56 -0300

    React Legado I - Componentização - Criando HTML e Utilizando Imagem

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import logoImg from './assets/images/logo.png'
 
 // Componente em classe é uma classe que herda a classe Component do React, e retorna HTML dentro do método render().
 class App extends React.Component {
   // Método responsável por renderizar o conteúdo HTML do nosso componente
   render() {
     return (
-      <header></header>
+      <header>
+        <nav>
+          <div className="nav-brand">
+            <img src={logoImg} alt="" />
+            <h1>Space Flight News</h1>
+          </div>
+
+          <ul className='nav-list'>
+            <li><a href="/">Home</a></li>
+            <li><a href="/">Trending</a></li>
+            <li><a href="/">Categories</a></li>
+            <li><a href="/">About us</a></li>
+          </ul>
+        </nav>
+      </header>
       )
   }
 }
