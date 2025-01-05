6045eda73d273d9c9f2acc4e1a00f8c49ed51e4b
ClecianoPedro <itzcleciano@gmail.com>
2025-01-05 00:11:45 -0300

    React Legado I - Componentização - Export Direto

--- React/my-app/src/App.jsx
+++ React/my-app/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from './components/Navbar/Navbar.jsx';
+import { Navbar } from './components/Navbar/Navbar.jsx';
 
 // Componente em classe é uma classe que herda a classe Component do React, e retorna HTML dentro do método render().
 class App extends React.Component {
